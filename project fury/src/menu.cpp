73cca4eb2bff4637e7b121caa4b15978a0928572
Freeky _gmr <junyed.alam.312006@gmail.com>
2026-04-03 21:53:58 +0200

    v2.0.1

--- project fury/src/menu.cpp
+++ project fury/src/menu.cpp
@@ -446,7 +446,7 @@ void menu::EndRender() noexcept {
 
 void menu::Render() noexcept {
 	ImDrawList* draw_list = ImGui::GetBackgroundDrawList();
-	draw_list->AddText(ImVec2(0, 0), ImU32(ImColor(145, 0, 0, 255)), "Fury Lite - v2.0.0");
+	draw_list->AddText(ImVec2(0, 0), ImU32(ImColor(145, 0, 0, 255)), "Fury Lite - v2.0.1");
 
 	ImVec2 bottom_left_screen(0, menu::height - 300.0f);
 	ImColor outline = ImColor(1, 0, 0, 255);
